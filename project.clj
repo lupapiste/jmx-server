a68acf3044dc6c790d935368dbc884fdb7a63518
Juha Jokimaki <juha.jokimaki@solita.fi>
2016-10-25 13:15:58 +0300

    Build with Leiningen

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject lupapiste/jmx-server "0.1.0"
+  :description "A Java library for creating a JXM server with SO_REUSEADDR socket option."
+  :url "https://www.lupapiste.fi/"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+  :scm {:name "git", :url "https://github.com/lupapiste/jmx-server.git"}
+  :java-source-paths ["src/java"]
+  :min-lein-version "2.5.0")
